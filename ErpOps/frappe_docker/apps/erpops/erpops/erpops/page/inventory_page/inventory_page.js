8042d63877a80c40d987351519c09ab5912378cd
Sarath Babu <sarathyadav112@gmail.com>
2026-06-15 13:01:23 +0530

    Rename inventory page to inventory_page, fix route interceptor collision, and push updates

--- ErpOps/frappe_docker/apps/erpops/erpops/erpops/page/inventory_page/inventory_page.js
+++ ErpOps/frappe_docker/apps/erpops/erpops/erpops/page/inventory_page/inventory_page.js
@@ -1,4 +1,4 @@
-frappe.pages['inventory'].on_page_load = function(wrapper) {
+frappe.pages['inventory_page'].on_page_load = function(wrapper) {
 	var page = frappe.ui.make_app_page({
 		parent: wrapper,
 		title: 'Inventory',
--- ErpOps/frappe_docker/apps/erpops/erpops/public/js/erpops.js
+++ ErpOps/frappe_docker/apps/erpops/erpops/public/js/erpops.js
@@ -3,14 +3,14 @@
 $(document).on('app_ready', function() {
     frappe.router.on('change', function() {
         var route = frappe.get_route_str();
-        if (route === 'workspace/Inventory' || route === 'workspace/inventory' || route === 'workspace/Inventory Ws') {
-            frappe.set_route('inventory');
+        if (route === 'inventory' || route === 'workspace/Inventory' || route === 'workspace/inventory' || route === 'workspace/Inventory Ws') {
+            frappe.set_route('inventory_page');
         }
     });
     
     // Check initial route on load
     var route = frappe.get_route_str();
-    if (route === 'workspace/Inventory' || route === 'workspace/inventory' || route === 'workspace/Inventory Ws') {
-        frappe.set_route('inventory');
+    if (route === 'inventory' || route === 'workspace/Inventory' || route === 'workspace/inventory' || route === 'workspace/Inventory Ws') {
+        frappe.set_route('inventory_page');
     }
 });
