1d55ec0bf64a7724023b448aa000e9a063213d36
Sarath Babu <sarathyadav112@gmail.com>
2026-06-15 13:04:26 +0530

    Update erpops.js route check to run immediately

--- ErpOps/frappe_docker/apps/erpops/erpops/public/js/erpops.js
+++ ErpOps/frappe_docker/apps/erpops/erpops/public/js/erpops.js
@@ -1,16 +1,18 @@
 /* Global client script for ErpOps custom route interception */
 
-$(document).on('app_ready', function() {
-    frappe.router.on('change', function() {
-        var route = frappe.get_route_str();
-        if (route === 'inventory' || route === 'workspace/Inventory' || route === 'workspace/inventory' || route === 'workspace/Inventory Ws') {
-            frappe.set_route('inventory_page');
-        }
-    });
-    
-    // Check initial route on load
+frappe.router.on('change', function() {
     var route = frappe.get_route_str();
     if (route === 'inventory' || route === 'workspace/Inventory' || route === 'workspace/inventory' || route === 'workspace/Inventory Ws') {
         frappe.set_route('inventory_page');
     }
+});
+
+// Run check immediately on load
+$(document).ready(function() {
+    setTimeout(function() {
+        var route = frappe.get_route_str();
+        if (route === 'inventory' || route === 'workspace/Inventory' || route === 'workspace/inventory' || route === 'workspace/Inventory Ws') {
+            frappe.set_route('inventory_page');
+        }
+    }, 300);
 });
